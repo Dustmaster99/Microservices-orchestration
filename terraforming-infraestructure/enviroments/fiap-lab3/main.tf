7e1c2d5329144bb232d0606ff07ae03aa94e520e
ZEC <eos.josehenrique@gmail.com>
2026-03-24 21:20:00 -0300

    add more resources to nodes

--- terraforming-infraestructure/enviroments/fiap-lab3/main.tf
+++ terraforming-infraestructure/enviroments/fiap-lab3/main.tf
@@ -114,13 +114,13 @@ module "eks" {
   node_group_name    = "microservices-eks-nodes"
   private_subnet_ids = module.vpc.private_subnets
 
-  instance_types = ["t3.small"]
+  instance_types = ["t3.medium"]
   capacity_type  = "SPOT"
   disk_size      = 20
 
-  desired_size = 2
-  min_size     = 1
-  max_size     = 3
+  desired_size = 3
+  min_size     = 2
+  max_size     = 4
 
   max_unavailable = 1
 
